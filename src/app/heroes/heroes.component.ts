468225e777bf25c8bbd6dca0266ca392abab7f18
Maciej Gardygajło <mg@idesign.center>
2018-06-18 10:38:34 +0200

    used RxJS of() to return an observable of mock heroes (Observable<Hero[]>)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,19 +8,26 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
   selectedHero: Hero;
 
-  constructor() {
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) {
 
   }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     console.log(hero);
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(res => this.heroes = res);
   }
 
 }
